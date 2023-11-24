cb6d3fc77c341b6f7880124f7d973b9612fabe6d
Midhat Perenda <midhat.perenda97@gmail.com>
2023-11-24 21:08:30 +0100

    Avatar group component

---
+++ components/ui/avatar-group.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { cva, type VariantProps } from 'class-variance-authority';
+
+import { cn } from '@/lib/utils';
+
+const avatarGroupVariants = cva('inline-flex -space-x-4 [&>span:hover]:z-10 [&>span>div:first-child:hover]:shadow-lg', {
+  variants: {
+    overlap: {
+      fromLeft: 'flex-row-reverse space-x-reverse',
+      fromRight: 'flex-row',
+    },
+  },
+  defaultVariants: {
+    overlap: 'fromRight',
+  },
+});
+
+interface AvatarGroupProps 
+  extends React.HTMLAttributes<HTMLDivElement>, 
+    VariantProps<typeof avatarGroupVariants> {}
+
+const AvatarGroup: React.FC<AvatarGroupProps> = ({ className, overlap, ...props }) => (
+  <div className={cn(avatarGroupVariants({ overlap }), className)} {...props} />
+);
+
+export { AvatarGroup };
--- components/ui/avatar.tsx
+++ components/ui/avatar.tsx
@@ -6,12 +6,15 @@ import { cva, type VariantProps } from 'class-variance-authority';
 
 import { cn } from '@/lib/utils';
 
-const avatarVariants = cva('relative flex h-10 w-10 shrink-0', {
+const avatarVariants = cva('relative flex h-10 w-10 shrink-0 rounded-lg', {
   variants: {
     size: {
       sm: 'h-10 w-10',
       md: 'h-12 w-12',
       lg: 'h-14 w-14',
+    },
+    rounded: {
+      true: 'rounded-full'
     }
   },
   defaultVariants: {
@@ -21,7 +24,6 @@ const avatarVariants = cva('relative flex h-10 w-10 shrink-0', {
 
 interface AvatarProps 
   extends VariantProps<typeof avatarVariants> {
-  rounded?: boolean,
   src?: string;
   alt?: string;
   fallback?: React.ReactNode;
@@ -36,21 +38,19 @@ const Avatar = React.forwardRef<
 >(({ className, size, rounded, src, alt = '', fallback, delayMs, action, ...props }, ref) => (
   <AvatarPrimitive.Root
     ref={ref}
-    className={cn(avatarVariants({ size, className }))}
+    className={cn(avatarVariants({ size, rounded, className }))}
     {...props}
   >
     <div className={`overflow-hidden h-full w-full border-2 ${rounded ? 'rounded-full' : 'rounded-lg'}`}>
-      {src &&
-        <AvatarPrimitive.Image
-          className='aspect-square h-full w-full object-cover'
-          src={src}
-          alt={alt}
-        />
-      }
+      <AvatarPrimitive.Image
+        className='aspect-square h-full w-full object-cover'
+        src={src}
+        alt={alt}
+      />
 
       {fallback &&
         <AvatarPrimitive.Fallback
-          className='flex h-full w-full items-center justify-center bg-secondary-200' 
+          className='flex h-full w-full items-center justify-center bg-secondary-100' 
           delayMs={delayMs}
         >
           {fallback}
@@ -67,4 +67,4 @@ const Avatar = React.forwardRef<
 ));
 Avatar.displayName = AvatarPrimitive.Root.displayName;
 
-export { Avatar };
+export { Avatar, avatarVariants };
